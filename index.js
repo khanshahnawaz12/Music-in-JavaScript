efe8c4c1e1381a832893e4a27aa23b6ac755cd99
shahnawaz <khanshahnawaz1947@gmail.com>
2021-11-26 18:42:21 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+    }
+    );
+}
+
