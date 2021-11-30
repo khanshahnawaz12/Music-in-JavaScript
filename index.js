f8d9d06ddd0801c5758d274d6425518a30495dec
shahnawaz <khanshahnawaz1947@gmail.com>
2021-12-01 02:08:32 +0530

    szk

--- index.js
+++ index.js
@@ -6,46 +6,49 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
             // var audio = new Audio('sounds/kick-bass.mp3');
             // audio.play();
             var buttonInnerHTML = this.innerHTML;
-            switch (buttonInnerHTML) {
-                case "Sa":
-                    var tom1 = new Audio('sounds/tom-1.mp3');
-                    tom1.play();
-                    break;
 
-                case "Re":
-                    var tom2 = new Audio('sounds/tom-2.mp3');
-                    tom2.play();
-                    break;
-
-                case "Ga":
-                    var tom3 = new Audio('sounds/tom-3.mp3');
-                    tom3.play();
-                    break;
-
-                case "Ma":
-                    var tom4 = new Audio('sounds/tom-4.mp3');
-                    tom4.play();
-                    break;
-
-                case "Pa":
-                    var snare = new Audio('sounds/snare.mp3');
-                    snare.play();
-                    break;
-
-                case "Da":
-                    var crash = new Audio('sounds/crash.mp3');
-                    crash.play();
-                    break;
-
-                case "Ni":
-                    var kick = new Audio('sounds/kick-bass.mp3');
-                    kick.play();
-                    break;
-
-                default: console.log(buttonInnerHTML);
-                    break;
-            }
         }
     );
 }
 
+function makeSound(key) {
+    switch (buttonInnerHTML) {
+        case "Sa":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "Re":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+        case "Ga":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+
+        case "Ma":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+
+        case "Pa":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "Da":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "Ni":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
